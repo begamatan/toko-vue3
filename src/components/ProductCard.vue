f0100ee1f63af5c22e90c32fe84a2d0854589af8
Agung Putra <agungptr97@gmail.com>
2022-09-01 21:48:08 +0700

    extract product card to component

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="card">
+        <img :src="product.get_thumbnail" class="card-img-top product-thumbnail" :alt="product.title">
+        <div class="card-body">
+        <h6 class="card-title">{{ product.title }}</h6>
+        <p class="card-text">{{ product.description.substr(0, 100) }}</p>
+        <router-link :to="product.get_absolute_url">
+            <button class="btn btn-primary">
+            Detail
+            </button>
+        </router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            product: {
+                required: true,
+                type: Object
+            }
+        }
+    }
+</script>
+
+<style>
+    .product-thumbnail {
+        width: 100%;
+        height: 400px;
+    }
+</style>
--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -8,19 +8,8 @@
         <div class="col mt-3">
           <h3>{{ category.title }}</h3>
           <div class="row">
-            <div class="col-3" v-for="product in category.products" :key="product.id">
-              <div class="card" style="width: 18rem;">
-                <img :src="product.get_image" class="card-img-top" :alt="product.title">
-                <div class="card-body">
-                  <h5 class="card-title">{{ product.title }}</h5>
-                  <p class="card-text">{{ product.description }}</p>
-                  <router-link :to="product.get_absolute_url">
-                    <button class="btn btn-primary">
-                      Detail
-                    </button>
-                  </router-link>
-                </div>
-              </div>
+            <div class="col-sm-3" v-for="product in category.products" :key="product.id">
+              <product-card :product="product"></product-card>
             </div>
           </div>
         </div>
@@ -30,8 +19,12 @@
   
   <script>
     import axios from 'axios'
+    import ProductCard from '../components/ProductCard.vue'
   
     export default {
+      components: {
+        ProductCard
+      },
       data() {
         return {
           category: {
